35bfb9a2b0034ed0543a3728703437c53d5c9d44
o.drapeza <o.drapeza@tinkoff.ru>
2021-09-04 19:02:13 +0300

    add vendor script

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,3 @@
-document.getElementById('app').innerText = 'Local script';
-throw 123;
+if (window.GLOBAL_DEPENDENCY) {
+  document.getElementById('app').innerText = `Loaded after vendor.js, GLOBAL_DEPENDENCY is ${window.GLOBAL_DEPENDENCY}`;
+}
